d568ef100022adc1a3411925851554363edec7b6
Juno <juno@clearsumm.it>
2023-04-23 17:50:12 +0700

    test: test form component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,24 +1,44 @@
-import * as React from 'react';
+import React from 'react'
 
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TextInput } from 'react-native'
+import { Form, Metrics } from 'react-native-formik-helper'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {}
-    </View>
-  );
+    <ScrollView keyboardShouldPersistTaps="handled" style={styles.container} contentContainerStyle={styles.content}>
+      <Form
+        initialValues={{}}
+        onSubmit={() => {}}
+        submitButtonTitle="Confirm"
+        Fields={
+          <>
+            <Text>Hello world</Text>
+            <TextInput style={styles.field} />
+            <Text>Hello world</Text>
+            <TextInput style={styles.field} />
+            <Text>Hello world</Text>
+            <TextInput style={styles.field} />
+          </>
+        }
+      />
+    </ScrollView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  content: {
+    paddingVertical: Metrics.xxl,
+    paddingHorizontal: Metrics.small,
+    height: '100%',
   },
-});
+  field: {
+    marginVertical: Metrics.xxs,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: Metrics.tiny,
+    height: Metrics.iconHeight * 2,
+    paddingHorizontal: Metrics.small,
+  },
+})
